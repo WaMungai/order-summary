005ecced979047246791de286949d1b894281139
The Passionate Programmer <wamungai10@gmail.com>
2023-12-13 21:59:54 +0300

    Include images and html structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import background from ".././src/images/illustration-hero.svg"
+import music from ".././src/images/icon-music.svg"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+    <section className="showcase">
+      <div className="background-image"></div>
+      <div className="card">
+        <img src={background} alt="hero patter" className="hero" />
+        <h1>Order Summary</h1>
+        <p>You can now listen to millions of songs,audio books, and podcasts on 
+          any device anywhere you like!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="pricing">
+          <div>
+            <img src={music} alt="music button" title="Play Music" />
+            <article>
+              <h3>Annual plan</h3>
+              <p>$59.99/year</p>
+            </article>
+          </div>
+          <button className="btn btn-change">Change</button>
+        </div>
+        <div className="buttons">
+          <button className="btn btn-primary">Proceed to payment</button>
+          <button className="btn btn-secondary">Cancek Order</button>
+        </div>
+         </div>
+         <div class="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopenner noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://joycemungai.vercel.app/"
+            target="_blank"
+            rel="noopenner noreferrer"
+          >
+            Fairy Tech Mother
+          </a>
+          .
+        </div>
+
+    </section>
+    </>
   );
 }
 
